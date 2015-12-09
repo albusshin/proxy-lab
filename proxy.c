7dde1eaf264f2411f4c53578ee99a1a25febd329
Tian Xin <txin@cmu.edu>
2015-12-09 00:26:36 -0500

    70

--- proxy.c
+++ proxy.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "csapp.h"
 
+//TODO handle interrupts and crashes and SIGPIPE handler
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
@@ -22,43 +23,52 @@ typedef struct cache_node_type {
     char *absolute_uri;
     char *content;
     size_t size;
+    time_t timestamp;
     struct cache_node_type *next;
     struct cache_node_type *prev;
 } CacheNode;
 
 CacheNode *cache_head = NULL;
 CacheNode *cache_tail = NULL;
-sem_t cache_mutex;
 
 size_t cache_size;
+sem_t cache_mutex, cache_w;
+int reader_count;
 
 CacheNode *find_cache_node(char *absolute_uri) {
-    P(&cache_mutex);
     CacheNode *p = cache_head;
     while (p) {
         if (!strcmp(absolute_uri, p -> absolute_uri)) {
-            V(&cache_mutex);
             return p;
         }
         p = p -> next;
     }
-    V(&cache_mutex);
     return NULL;
 }
 
-void promote(char *absolute_uri) {
-    //TODO
-}
-
 CacheNode *get_cache(char *absolute_uri) {
-    //TODO
     P(&cache_mutex);
-    return find_cache_node(absolute_uri);
+    reader_count++;
+    if (reader_count == 1) { /* First in */
+        P(&cache_w);
+    }
     V(&cache_mutex);
+
+    CacheNode * ret = find_cache_node(absolute_uri);
+    if (ret) {
+        ret -> timestamp = time(NULL);
+    }
+
+    P(&cache_mutex);
+    reader_count--;
+    if (reader_count == 0) { /* Last out */
+        V(&cache_w);
+    }
+    V(&cache_mutex);
+    return ret;
 }
 
 void delete_cache_node(CacheNode *cache_node) {
-    P(&cache_mutex);
     if (cache_node == cache_head) {
         cache_head = cache_head -> next;
     }
@@ -72,22 +82,39 @@ void delete_cache_node(CacheNode *cache_node) {
         /* cache_node is tail */
         cache_tail = cache_tail -> prev;
     }
-    V(&cache_mutex);
     free(cache_node -> absolute_uri);
     free(cache_node -> content);
     free(cache_node);
 }
 
+void evict_cache() {
+    CacheNode *p, *to_evict = cache_head;
+    for (p = cache_head; p; p = p -> next) {
+        if (p -> timestamp < to_evict -> timestamp) {
+            to_evict = p;
+        }
+    }
+    printf("Cache evict, URI:%s\n", to_evict -> absolute_uri);
+    cache_size -= to_evict -> size;
+    delete_cache_node(to_evict);
+}
+
 //TODO deal with all malloc with smaller case 'm''s, and other stuff that would crash the proxy.
 void put_cache(char *absolute_uri, char *content, size_t size) {
+    P(&cache_w);
+    cache_size += size;
+    while (cache_size > MAX_CACHE_SIZE) {
+        evict_cache();
+    }
     CacheNode *cache_node;
-    P(&cache_mutex);
-    if ((cache_node = get_cache(absolute_uri)) != NULL) {
+    if ((cache_node = find_cache_node(absolute_uri)) != NULL) {
         delete_cache_node(cache_node);
     }
 
     /* Inserting cache_node to head */
     cache_node = malloc(sizeof(CacheNode));
+    //TODO update time() to be fail safe
+    cache_node -> timestamp = time(NULL);
     cache_node -> next = cache_head;
     cache_node -> prev = NULL;
     if (cache_node -> next) {
@@ -101,7 +128,7 @@ void put_cache(char *absolute_uri, char *content, size_t size) {
     cache_node -> absolute_uri = absolute_uri;
     cache_node -> content = content;
     cache_node -> size = size;
-    V(&cache_mutex);
+    V(&cache_w);
 }
 
 /*****************Helpers*******************/
@@ -122,12 +149,20 @@ void serve_proxy(ProxyInfo *proxy_info) {
     char *uri = proxy_info -> uri;
     rio_t *p_server_rio = proxy_info -> p_server_rio;
 
-    printf("hostname: %s, port: %s\n, uri: %s\n", hostname, port, uri);
+    char *cache_absolute_uri = (char *) malloc(MAXLINE);
+    sprintf(cache_absolute_uri, "%s:%s%s", hostname, port, uri);
+
+    CacheNode *cache_node;
+    if ((cache_node = get_cache(cache_absolute_uri)) != NULL) {
+        printf("Cache hit, URI: %s\n", cache_node -> absolute_uri);
+        Rio_writen(fd, cache_node -> content, cache_node -> size);
+        return;
+    }
+
     clientfd = Open_clientfd(hostname, port);
     Rio_readinitb(&rio, clientfd);
 
     /* trasmit first line of request */
-    //strcpy(buf, "GET http://www.example.com HTTP/1.0\r\n\r\n");
     sprintf(buf, "GET %s HTTP/1.0\r\n", uri);
     Rio_writen(clientfd, buf, strlen(buf));
 
@@ -157,44 +192,37 @@ void serve_proxy(ProxyInfo *proxy_info) {
         }
 
         Rio_writen(clientfd, buf, strlen(buf));
-	    printf("%s", buf);
     }
     /* host header */
     if (!host_set) {
         sprintf(buf, "Host: %s", hostname);
         Rio_writen(clientfd, buf, strlen(buf));
-	    printf("%s", buf);
     }
     /* user agent header */
     Rio_writen(clientfd, (char *) user_agent_hdr, strlen(user_agent_hdr));
-	printf("%s", user_agent_hdr);
     /* connection header */
     Rio_writen(clientfd, (char *) connection_hdr, strlen(connection_hdr));
-	printf("%s", connection_hdr);
     /* proxy connection header */
     Rio_writen(clientfd, (char *) proxy_connection_hdr, strlen(proxy_connection_hdr));
-	printf("%s", proxy_connection_hdr);
 
     char *cache_content = (char *) malloc(MAX_OBJECT_SIZE);
     char *cachep = cache_content;
-    char *cache_absolute_uri = (char *) malloc(MAXLINE);
     // TODO use strncpys
     // TODO try to exploit your own program! It'll be fun.
-    strncpy(cache_absolute_uri, , MAXLINE);
-
-    put_cache(
     size_t s = 0;
+    size_t cache_object_size = 0;
     while ((s = Rio_readnb(&rio, buf, MAXLINE)) && s != -1) {
         Rio_writen(fd, buf, s);
-        memcpy(cachep, buf, s);
-        cachep += s;
-        Fputs(buf, stdout);
+        cache_object_size += s;
+        if (cache_object_size <= MAX_OBJECT_SIZE) {
+            memcpy(cachep, buf, s);
+            cachep += s;
+        }
     }
-
-    Fputs("Closed.", stdout);
+    if (cache_object_size <= MAX_OBJECT_SIZE) {
+        put_cache(cache_absolute_uri, cache_content, cache_object_size);
+    }
     Close(clientfd);
-
-    return;
 }
 
 void parseUri(char *requestUri, char *hostname, char *port, char *uri) {
@@ -269,6 +297,7 @@ void doit(int fd) {
     if (strcasecmp(method, "GET")) {
         clienterror(fd, method, "501", "Not Implemented",
                     "Proxy  does not implement this method");
+        printf("Rejected method %s\n", method);
         return;
     }
     //TODO clean memory in each thread
@@ -300,6 +329,8 @@ main(int argc, char **argv) {
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
+    V(&cache_mutex);
+    V(&cache_w);
     pthread_t tid;
 
     /* Check command line args */
